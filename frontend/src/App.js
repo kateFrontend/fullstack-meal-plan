44ec4ebf8696639ab830417cf4ce4149858517ca
kateFrontend <ecdanilova@gmail.com>
2023-03-03 13:13:49 -0500

    add main style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>TEST</h1>
+      <h1>Meal Plan</h1>
+      <input type="text" placeholder="Add a meal"></input>
+      <button>Add</button>
+      <p>Test</p>
     </div>
   );
 }
