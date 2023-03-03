a2d618ee90b56faf6f5d586e98d6026d6b6629d5
kateFrontend <ecdanilova@gmail.com>
2023-03-03 11:04:32 -0500

    connect with MondoDb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
-const PORT = 8000;
+mongoose.set("strictQuery", false);
+require('dotenv').config();
+
+mongoose
+.connect(process.env.MONGODB_LINK)
+.then(() => console.log("We were connectiong to MongoDB"))
+.catch((err) => console.log(err))
+
+const PORT = 8000 || process.env.port
 
 app.listen(PORT, () => {
     console.log(`I'm listenning on port ${PORT}`)
