ab02aef39b473855c1c94477b2e8d35b5a67ebff
kateFrontend <ecdanilova@gmail.com>
2023-03-04 13:49:00 -0500

    display input data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,33 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import MyMeals from "./MyMeals";
-import { getAllMeals } from "./FetchMeals";
+import { getAllMeals, addMeal } from "./FetchMeals";
 
 function App() {
-
-  const [myMeal, setMeal] = useState([])
+  const [myMeal, setMeal] = useState([]);
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
-    getAllMeals(setMeal)
-  },[])
-
+    getAllMeals(setMeal);
+  }, []);
 
   return (
     <div>
       <h1>Meal Plan</h1>
-      <input type="text" placeholder="Add a meal"></input>
-      <button>Add</button>
+      <input
+      type="text"
+      placeholder="Add a meal"
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+      />
+    
+      <button onClick={() => addMeal(title,setTitle,setMeal)}>Add</button>
 
       {/* <MyMeals text="We got here"/> */}
 
-    {myMeal.map((meal) => <MyMeals text={meal.title} key={meal.id}/>)}
-
+      {myMeal.map((meal) => (
+        <MyMeals text={meal.title} key={meal.id} />
+      ))}
     </div>
   );
 }
--- frontend/src/FetchMeals.js
+++ frontend/src/FetchMeals.js
@@ -3,8 +3,16 @@ import axios from "axios";
 const getAllMeals = (setMeal) => {
   axios.get("http://localhost:7000").then(({ data }) => {
     console.log(data);
-    setMeal(data)
+    setMeal(data);
   });
 };
 
-export { getAllMeals }
+const addMeal = (title, setTitle, setMeal) => {
+  axios.post(`http://localhost:7000/saveMeals`, { title }).then((data) => {
+    console.log(data);
+    setTitle("");
+    getAllMeals(setMeal);
+  });
+};
+
+export { getAllMeals, addMeal };
